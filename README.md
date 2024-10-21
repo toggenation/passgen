ecc08ce3ce74617633502a3f3b3e4be0e6e42170
James McDonald <james@toggen.com.au>
2024-10-21 13:22:38 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Simple Password Generator
+
+Usage:
+
+composer gen [password_length]
+
+Eg:
+
+```sh
+composer gen 12
+```
+
+## Install
+```sh
+git clone $this_repo_url
+cd passgen
+composer install
+composer gen
+```
---
+++ src/Passgen.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Toggenation\Passgen;
+
+use Composer\Script\Event;
+
+class Passgen
+{
+    public const DEFAULT_LENGTH = 9;
+
+    public static function getLength(array $args): int
+    {
+        $length = $args && is_numeric($args[0]) ? $args[0] : self::DEFAULT_LENGTH;
+
+        return (int) $length;
+    }
+
+    public static function generate(Event $event)
+    {
+
+        $length = self::getLength($event->getArguments());
+
+        $factory = new \RandomLib\Factory;
+
+        $generator = $factory->getHighStrengthGenerator();
+
+        $pwd = $generator->generateString($length);
+
+        echo $pwd . PHP_EOL;
+
+        return $pwd;
+    }
+}
